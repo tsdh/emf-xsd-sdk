fa0ef8f455663df00b8eace613ff18cd8c85b327
Tassilo Horn <tsdh@gnu.org>
2014-10-31 21:22:29 +0100

    Add License

--- project.clj
+++ project.clj
@@ -6,15 +6,12 @@
   This is an inofficial bundle of EMF release JARs for simple usage by
   Maven-based projects."
   :url "https://github.com/tsdh/emf-xsd-sdk"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   ;; Where are the EMF class files?
   :filespecs [{:type :path :path "emf-jars"}]
   :jar-exclusions [#"(PUT_EMF_SDK_JARS_HERE|toc.xml|readme.html)"
                    #"about\.(html|ini|properties|mappings)"
                    #"plugin\.(xml|properties)"
                    #".*\.(sf|dsa|rsa|SF|RSA|DSA|jar|options|api_description|java|gif|png)$"
-                   #"^(\.git/|examples/|tutorials/|cheatsheets/|references/|index/|schema/|rose/|images/|icons/|css/|templates/|cache/|model/|about_files/)"]
-  :uberjar-exclusions [#"(PUT_EMF_SDK_JARS_HERE|toc.xml|readme.html)"
-                       #"about\.(html|ini|properties|mappings)"
-                       #"plugin\.(xml|properties)"
-                       #".*\.(sf|dsa|rsa|SF|RSA|DSA|jar|options|api_description|java|gif|png)$"
-                       #"^(\.git/|examples/|tutorials/|cheatsheets/|references/|index/|schema/|rose/|images/|icons/|css/|templates/|cache/|model/|about_files/)"])
+                   #"^(\.git/|examples/|tutorials/|cheatsheets/|references/|index/|schema/|rose/|images/|icons/|css/|templates/|cache/|model/|about_files/)"])
